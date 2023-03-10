9bcdfbcc48d2eb9a9b20f254b2f587d769aa6d5d
RoxaneKirchmeyer <roxane.kirchmeyer@gmail.com>
2023-03-10 13:52:02 +0100

    menu burger page itineraire

--- Javascript/menuBurger.js
+++ Javascript/menuBurger.js
@@ -2,9 +2,14 @@ let btnBurger = document.querySelector("#btn-burger");
 
 let navBar = document.querySelector("#navIndex");
 
+let navItineraire = document.querySelector(".navShadow");
+
 
 btnBurger.addEventListener("click", () => {
     // Code à effectuer si l'utilisateur clique sur btnBurger
     navBar.classList.toggle("visible");
 }
 )
+btnBurger.addEventListener("click", () => {
+    navItineraire.classList.toggle("visible");
+})
